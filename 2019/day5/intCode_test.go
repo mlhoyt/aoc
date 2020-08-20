e0fa73c6e79fab9e0e64d1fd2700caa80a2801a5
Mike Hoyt <mhoyt@shujinko.io>
2020-08-20 07:23:51 -0700

    unsuccessful attempt to fix TestRunOpOutput

--- 2019/day5/intCode_test.go
+++ 2019/day5/intCode_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,7 +14,7 @@ func TestRunOpAddIndirect(t *testing.T) {
 	code := []int{1, 2, 3, 5, 99, 0}
 	expected := []int{1, 2, 3, 5, 99, 8}
 
-	intcode := NewIntCode(code, os.Stdin)
+	intcode := NewIntCode(code, os.Stdin, os.Stdout)
 	err := intcode.Run()
 	if err != nil {
 		t.Error(err)
@@ -25,7 +27,7 @@ func TestRunOpMultIndirect(t *testing.T) {
 	code := []int{2, 2, 3, 5, 99, 0}
 	expected := []int{2, 2, 3, 5, 99, 15}
 
-	intcode := NewIntCode(code, os.Stdin)
+	intcode := NewIntCode(code, os.Stdin, os.Stdout)
 	err := intcode.Run()
 	if err != nil {
 		t.Error(err)
@@ -38,7 +40,7 @@ func TestRunOpAddIndirectMultIndirect(t *testing.T) {
 	code := []int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}
 	expected := []int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}
 
-	intcode := NewIntCode(code, os.Stdin)
+	intcode := NewIntCode(code, os.Stdin, os.Stdout)
 	err := intcode.Run()
 	if err != nil {
 		t.Error(err)
@@ -54,7 +56,7 @@ func TestRunOpInput(t *testing.T) {
 	var stdin bytes.Buffer
 	stdin.Write([]byte("13\n"))
 
-	intcode := NewIntCode(code, &stdin)
+	intcode := NewIntCode(code, &stdin, os.Stdout)
 	err := intcode.Run()
 	if err != nil {
 		t.Error(err)
@@ -64,15 +66,17 @@ func TestRunOpInput(t *testing.T) {
 }
 
 func TestRunOpOutput(t *testing.T) {
-	t.Skip() // FIXME: need to control stdout and read 13
-	code := []int{3, 3, 99, 13}
-	expected := []int{3, 3, 99, 13}
+	code := []int{4, 3, 99, 13}
+	expected := []int{4, 3, 99, 13}
 
-	intcode := NewIntCode(code, os.Stdin)
+	var stdout bytes.Buffer
+
+	intcode := NewIntCode(code, os.Stdin, &stdout)
 	err := intcode.Run()
 	if err != nil {
 		t.Error(err)
 	}
 
 	assert.Equal(t, expected, intcode.Code())
+	assert.Equal(t, []byte("13\n"), stdout.Bytes())
 }
