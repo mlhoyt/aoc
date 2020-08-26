be1a3c50fbbff9bfa9d0fedca7d388f575eec85c
Mike Hoyt <mhoyt@shujinko.io>
2020-08-26 08:46:45 -0700

    update main.go for part #2

--- 2019/day6/main.go
+++ 2019/day6/main.go
@@ -12,6 +12,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	checkSum := uom.CheckSum()
-	fmt.Printf("checkSum %d\n", checkSum)
+	// checkSum := uom.CheckSum()
+	// fmt.Printf("checkSum %d\n", checkSum)
+
+	youOrbits := uom.GetOrbiteeList("YOU")
+	sanOrbits := uom.GetOrbiteeList("SAN")
+	commonOrbitee := youOrbits.CalculateDivergencePoint(sanOrbits)
+
+	youOrbitalDist, err := uom.GetOrbitalDistance("YOU", commonOrbitee)
+	if err != nil {
+		fmt.Printf("[ERROR] %v\n", err)
+		os.Exit(1)
+	}
+	sanOrbitalDist, err := uom.GetOrbitalDistance("SAN", commonOrbitee)
+	if err != nil {
+		fmt.Printf("[ERROR] %v\n", err)
+		os.Exit(1)
+	}
+	orbitalTransfers := youOrbitalDist + sanOrbitalDist
+	fmt.Printf("orbital-transfers %d\n", orbitalTransfers)
 }
