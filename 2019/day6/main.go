7c7ed7a0303609c2200ba357f074bcbb29b86690
Mike Hoyt <mhoyt@shujinko.io>
2020-08-28 07:55:57 -0700

    update main.go orbital distance algorithm

--- 2019/day6/main.go
+++ 2019/day6/main.go
@@ -12,23 +12,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	// checkSum := uom.CheckSum()
-	// fmt.Printf("checkSum %d\n", checkSum)
-
 	youOrbits := uom.GetOrbiteeList("YOU")
 	sanOrbits := uom.GetOrbiteeList("SAN")
-	commonOrbitee := youOrbits.CalculateDivergencePoint(sanOrbits)
+	commonOrbiteeIndex := youOrbits.CalculateDivergencePoint(sanOrbits)
 
-	youOrbitalDist, err := uom.GetOrbitalDistance("YOU", commonOrbitee)
-	if err != nil {
-		fmt.Printf("[ERROR] %v\n", err)
-		os.Exit(1)
-	}
-	sanOrbitalDist, err := uom.GetOrbitalDistance("SAN", commonOrbitee)
-	if err != nil {
-		fmt.Printf("[ERROR] %v\n", err)
-		os.Exit(1)
-	}
-	orbitalTransfers := youOrbitalDist + sanOrbitalDist
+	youOrbitalDistance := youOrbits.CalculateOrbitalDistance(commonOrbiteeIndex)
+	sanOrbitalDistance := sanOrbits.CalculateOrbitalDistance(commonOrbiteeIndex)
+
+	orbitalTransfers := youOrbitalDistance + sanOrbitalDistance
 	fmt.Printf("orbital-transfers %d\n", orbitalTransfers)
 }
